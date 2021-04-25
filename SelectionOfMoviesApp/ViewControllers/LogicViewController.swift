5bf09158ce0a53e767fc35916f56234c2757514b
GayaGaga <marcinevskaya93@mail.ru>
2021-04-26 00:16:42 +0300

    add autlet

--- SelectionOfMoviesApp/ViewControllers/LogicViewController.swift
+++ SelectionOfMoviesApp/ViewControllers/LogicViewController.swift
@@ -10,25 +10,36 @@ import UIKit
 class LogicViewController: UIViewController {
     
     
+    @IBOutlet var genreLabel: UIStackView!
+    @IBOutlet var chooseGenreButtons: [UIButton]!
+    
+    @IBOutlet var yearsLabel: UIStackView!
+    @IBOutlet var chooseYearsButtons: [UIButton]!
+    
+    @IBOutlet var ratingLabel: UIStackView!
+    @IBOutlet var chooseRatingButtons: [UIButton]!
     
     let film = Film.getFilms()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
 
     }
     
     
-    
+}
     
     // MARK: - Navigation
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //
 //    }
-}
+
 extension LogicViewController {
-    // private func setupUI() {
-//    for stack in [arrayStack]
-//    stack.isHidden = .true
-//}
+     private func setupUI() {
+        for stack in [genreLabel, yearsLabel, ratingLabel] {
+            stack?.isHidden = true
+        }
+     }
 }
