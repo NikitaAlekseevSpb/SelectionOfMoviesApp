70b749ff8057ce5116c6111eb5b3db6ecdcae7a7
GayaGaga <marcinevskaya93@mail.ru>
2021-04-25 18:32:25 +0300

    BeginLogicVC

---
+++ SelectionOfMoviesApp/ViewControllers/LogicViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogicViewController.swift
+//  SelectionOfMoviesApp
+//
+//  Created by Larisa on 25.04.2021.
+//
+
+import UIKit
+
+class LogicViewController: UIViewController {
+    
+    
+    
+    let film = Film.getFilms()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+}
